66ec583599b2580f69e7e5bce7674d573cdc0559
Eike Starkmann <eike.starkmann@posteo.de>
2026-01-06 17:46:40 +0100

    [TASK] move settings to plugin
    Ref: #28622

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,15 +3,28 @@
 declare(strict_types=1);
 
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility as ExtbaseExtensionUtility;
 
 (function (): void {
-
-    ExtensionUtility::registerPlugin(
-        'Easychat',
-        'EasychatFrontend',
-        // @TODO localize
-        'Easychat Frontend'
+    $pluginName = 'EasychatFrontend';
+    $extensionName = "easychat";
+    $upperCaseFirstPluginName = ucfirst($pluginName);
+    $upperCaseFirstExtensionName = ucfirst($extensionName);
+    $lowerCasedPluginName = strtolower($pluginName);
+    $lowerCasedExtensionName = strtolower($extensionName);
+    $extensionKey = GeneralUtility::camelCaseToLowerCaseUnderscored($extensionName);
+    ExtbaseExtensionUtility::registerPlugin(
+        $upperCaseFirstExtensionName,
+        $upperCaseFirstPluginName,
+        'LLL:EXT:'.$extensionKey.'/Resources/Private/Language/locallang_db.xlf:title.' . $pluginName
     );
-
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$lowerCasedExtensionName.'_' . $lowerCasedPluginName] = 'pi_flexform';
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$lowerCasedExtensionName.'_' . $lowerCasedPluginName] = 'recursive,select_key,pages';
+    ExtensionManagementUtility::addPiFlexFormValue(
+        $lowerCasedExtensionName.'_' . $lowerCasedPluginName,
+        'FILE:EXT:'.$extensionKey.'/Configuration/FlexForms/Easychat.xml'
+    );
 })();

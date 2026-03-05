753fcceeac58d15becbacdfcd7c553ec25b962e6
Thomas Alboth <ta@undkonsorten.com>
2026-03-05 16:51:52 +0100

    [TASK] Change description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,10 +2,8 @@
 
 /** @noinspection PhpUndefinedVariableInspection */
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Easychat: The ai chatbot/assistent from heaven',
-    'description' => 'A lightweight chatbot/assistant without third party chat tools.
-    All you need is TYPO3 and an LLM endpoint. Works with nearly any LLM (ollama, chatgpt, mistral, gemini etc)
-    Focused on privacy and data protection (DSGVO)',
+    'title' => 'EasyChat – TYPO3 standalone AI Chatbot',
+    'description' => 'A TYPO3 chatbot/assistant without third party tools. Only TYPO3 + LLM endpoint needed (ChatGpt, Mistral...). Privacy focused (GDPR). Chats stored in TYPO3 only.',
     'category' => 'Frontend Plugins',
     'author' => 'Eike Starkmann',
     'author_email' => 'es@undkonsorten.com',
